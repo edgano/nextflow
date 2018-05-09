9a31994758c704d96c1f26f826a2e3df4c581956
Edgar <edgano@gmail.com>
2018-05-09 13:59:18 +0200

    catch IN/OUT for RO

--- src/main/groovy/nextflow/Const.groovy
+++ src/main/groovy/nextflow/Const.groovy
@@ -59,12 +59,12 @@ class Const {
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1525862519301
+    static public final long APP_TIMESTAMP = 1524557352936
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 4860
+    static public final int APP_BUILDNUM = 4803
 
 
     /**
