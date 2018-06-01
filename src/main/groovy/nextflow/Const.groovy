2aec55122000d5f211a2b9b00dcb3e78bea7b0af
Edgar <edgano@gmail.com>
2018-06-01 16:59:07 +0200

    RO used & wasGeneratedBy

--- src/main/groovy/nextflow/Const.groovy
+++ src/main/groovy/nextflow/Const.groovy
@@ -59,12 +59,12 @@ class Const {
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1527859597630
+    static public final long APP_TIMESTAMP = 1527865006857
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 5182
+    static public final int APP_BUILDNUM = 5220
 
 
     /**
